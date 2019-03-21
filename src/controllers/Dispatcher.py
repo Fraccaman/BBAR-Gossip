a36f4609a047ca94334a3a62a9fbded7aa2bf317
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2019-03-21 14:21:35 +0100

    Refactor dispatcher

--- src/controllers/Dispatcher.py
+++ src/controllers/Dispatcher.py
@@ -9,16 +9,16 @@ from src.utils.Logger import Logger
 
 class Dispatcher:
 
-    def __init__(self, controllers=None):
-        self.controllers = [] if controllers is None else controllers
+    def __init__(self, controllers):
+        self.controllers = controllers
 
     @staticmethod
     def get_peer_dispatcher():
-        return Dispatcher([RegisterController])
+        return Dispatcher([RegisterController()])
 
     @staticmethod
     def get_bn_dispatcher():
-        return Dispatcher([HelloController])
+        return Dispatcher([HelloController()])
 
     @staticmethod
     def deserialize_data(msg_bytes) -> Message:
@@ -29,5 +29,5 @@ class Dispatcher:
         for controller in self.controllers:
             if controller.is_valid_controller_for(message):
                 Logger.get_instance().debug_item('A {} message is arrived and being handled'.format(message))
-                await controller().handle(connection, message)
+                await controller.handle(connection, message)
                 break
